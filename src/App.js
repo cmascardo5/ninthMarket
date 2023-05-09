44b36c7c1e440415ef6a38052f1c7f371b49b1ca
Christian M <cmascardo5@gmail.com>
2023-05-09 16:48:32 -0700

    added logo to home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './nine.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Website in development; follow our Instagram at
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/ninthmarket/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          https://www.instagram.com/ninthmarket/
         </a>
       </header>
     </div>
